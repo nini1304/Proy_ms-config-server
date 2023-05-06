9cac80f8f22516a5bb6693f8d114ceffe9325e8b
calc <geraldine.ibieta13@gmail.com>
2023-05-06 18:50:18 -0400

    Creacion del servidor de configuraciones

---
+++ Proy_ms-config-server/src/test/kotlin/com/example/Proy_msconfigserver/ProyMsConfigServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.Proy_msconfigserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProyMsConfigServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
