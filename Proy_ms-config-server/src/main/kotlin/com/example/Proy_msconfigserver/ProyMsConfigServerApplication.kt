a90447a38a85732bbd36e8c476aee97b8c0350ad
Mirko Alarcon <mirko.alarcon@ucb.edu.bo.com>
2023-05-06 20:59:13 -0400

    servidor correjido en privado

--- Proy_ms-config-server/src/main/kotlin/com/example/Proy_msconfigserver/ProyMsConfigServerApplication.kt
+++ Proy_ms-config-server/src/main/kotlin/com/example/Proy_msconfigserver/ProyMsConfigServerApplication.kt
@@ -4,10 +4,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.config.server.EnableConfigServer
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
 @EnableConfigServer
-@EnableDiscoveryClient
 class ProyMsConfigServerApplication
 
 fun main(args: Array<String>) {
